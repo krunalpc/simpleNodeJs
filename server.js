8385e74fa2dcba3abf40dbad6b259cc18ea24529
krunalpc <krunalcp@Live.com>
2015-11-10 19:06:18 -0500

    Portfolio Commit

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', handleRoot);
+// app.get('/', handleRoot);
 
-function handleRoot(req, res){
-  res.send('hello world !!!');
-}
+// function handleRoot(req, res){
+//   res.send('hello world !!!');
+// }
 
-app.listen(3000);
+app.use(express.static(__dirname+'/public'));
+
+// app.listen(3000);
+
+var host =process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port =process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+ app.listen(port , host);
